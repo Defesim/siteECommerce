b407755cef59cfe4c55be1849e2df81a099402be
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-07-17 17:13:55 +0200

    Probleme modif nb produits

--- src/com/intiformation/siteECommerce/controller/GestionPanierBean.java
+++ src/com/intiformation/siteECommerce/controller/GestionPanierBean.java
@@ -25,6 +25,9 @@ public class GestionPanierBean implements Serializable {
 	private List<Panier> listePanierBdd;
 	private Panier Panier;
 	private IPanierDAO PanierDAO;
+	private int quantiteModif;
+
+
 
 	public GestionPanierBean() {
 
@@ -168,7 +171,7 @@ public class GestionPanierBean implements Serializable {
 		 * la prop 'livre' du MB encapsule les infos du livre à modifier dans
 		 * la base de donnée
 		 */
-	UIComponent composantParam = event.getComponent().findComponent("updateID");
+		UIComponent composantParam = event.getComponent().findComponent("updateID");
 		
 		UIParameter cp = (UIParameter) composantParam;
 		
@@ -176,6 +179,9 @@ public class GestionPanierBean implements Serializable {
 		int PanierId = (int) cp.getValue();
 		
 		Panier panierAEditer = PanierDAO.getById(PanierId);
+		System.out.println(PanierId);
+		
+		panierAEditer.setQuantite(getQuantiteModif());
 		
 		// 1. recup du context de JSF
 		FacesContext contextJSF = FacesContext.getCurrentInstance();
@@ -276,5 +282,13 @@ public void supprimerPanier(ActionEvent event) {
 		this.listePanierBdd = listePanierBdd;
 	}
 	
+	public int getQuantiteModif() {
+		return quantiteModif;
+	}
+
+
+	public void setQuantiteModif(int quantiteModif) {
+		this.quantiteModif = quantiteModif;
+	}
 	
 }//end class
